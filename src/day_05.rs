9b19edb8de17aa8bf6f35d617063ecb8ddf9ac35
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2024-12-05 22:27:48 +0100

    feat: 🎄Day 5 part 2

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,6 +1,19 @@
 /*
     Part 1 is relatively simple, I just made a map/dict to slightly
     speed up the rules retrieval.
+
+    Part 2 was a bit more complex to do well. First-off, I didn't
+    know about the Kahn's algorithm until today, which is something.
+
+    Second, the test case was nice but the actual data was a bit nasty
+    with cycles in the rule set. I thought I was going crazy, but I
+    checked the r/adventofcode subreddit and someone mentionned that
+    the rules wern't a DAG until you only consider the page numbers of
+    a given update. The post finished with "That's just evil.", and I
+    think I agree because it's only day 5 :'(.
+
+    Anyway, a hard part 2 if you don't know about Kahn's algorithm or
+    tried to reuse the Kahn's outputs between updates.
 */
 
 use std::collections::VecDeque;
@@ -85,14 +98,19 @@ fn sort_with_lookup_table(data: &[u8], lookup_table: &[u8]) -> Vec<u8> {
     output
 }
 
+fn build_faster_rules(rules: &[(u8, u8)]) -> [Vec<u8>; 256] {
+    let mut faster_rules: [Vec<u8>; 256] = std::array::from_fn(|_| Vec::new());
+    for (a, b) in rules {
+        faster_rules[*b as usize].push(*a);
+    }
+    faster_rules
+}
+
 pub fn day_05_part_1(data: &str) -> i64 {
     let (_, data) = parse_input_data(data).expect("Failed to parse input data");
 
     // We create an array that for each page, contains the list of pages that should come before
-    let mut faster_rules: [Vec<u8>; 256] = std::array::from_fn(|_| Vec::new());
-    for (a, b) in data.rules {
-        faster_rules[b as usize].push(a);
-    }
+    let faster_rules = build_faster_rules(&data.rules);
 
     data.updates
         .iter()
@@ -101,28 +119,32 @@ pub fn day_05_part_1(data: &str) -> i64 {
         .sum()
 }
 
-pub fn day_05_part_2(data: &str) -> i64 {
-    let (_, data) = parse_input_data(data).expect("Failed to parse input data");
-
-    // We are going for a Kahn's algorithm approach. I started to come up with
-    // something looking vaguely like it, but it sounded hard, so I checked the solution.
-    // Sorry.
-    let mut faster_rules: [Vec<u8>; 256] = std::array::from_fn(|_| Vec::new());
+// We are going for a Kahn's algorithm approach. I started to come up with
+// something looking vaguely like it, but it sounded hard, so I checked the solution.
+// Sorry.
+fn kahn_algorithm(numbers: &[u8], rules: &[(u8, u8)]) -> (Vec<u8>, [u8; 256]) {
     let mut graph: [Vec<u8>; 256] = std::array::from_fn(|_| Vec::new());
     let mut in_degree: [u8; 256] = [0; 256];
-    for (a, b) in data.rules {
-        faster_rules[b as usize].push(a);
-        graph[a as usize].push(b);
-        in_degree[b as usize] += 1;
+
+    let mut number_presence = [false; 256];
+    for number in numbers {
+        number_presence[*number as usize] = true;
     }
 
-    // we could identify all the numbers, but we can also do 0-255 because it's simple
-    let mut queue: VecDeque<u8> = (0..256_usize)
-        .filter(|i| in_degree[*i] == 0)
-        .map(|i| i as u8)
+    for (a, b) in rules {
+        if number_presence[*a as usize] && number_presence[*b as usize] {
+            graph[*a as usize].push(*b);
+            in_degree[*b as usize] += 1;
+        }
+    }
+
+    let mut queue: VecDeque<u8> = numbers
+        .iter()
+        .filter(|n| in_degree[**n as usize] == 0)
+        .copied()
         .collect();
 
-    let mut sorted_order = Vec::with_capacity(256);
+    let mut sorted_order = Vec::with_capacity(numbers.len());
 
     while !queue.is_empty() {
         let current = queue.pop_front().expect("Queue is empty !");
@@ -135,18 +157,8 @@ pub fn day_05_part_2(data: &str) -> i64 {
             }
         }
     }
-    println!("{:?}", sorted_order);
 
-    if sorted_order.len() != 256 {
-        //cycle_candidates = [node for node in numbers if in_degree[node] > 0]
-        let cycle_candidates: Vec<u8> = (0..256_usize)
-            .filter(|node| in_degree[*node] > 0)
-            .map(|node| node as u8)
-            .collect();
-
-        println!("{:?}", cycle_candidates);
-        println!("{:?}", cycle_candidates.len());
-
+    if sorted_order.len() != numbers.len() {
         panic!("We didn't find all the pages ! A cycle is present !");
     }
 
@@ -156,13 +168,19 @@ pub fn day_05_part_2(data: &str) -> i64 {
         sort_lookup_table[*page as usize] = i as u8;
     }
 
-    println!("{:?}", sorted_order);
-    println!("{:?}", sort_lookup_table);
+    (sorted_order, sort_lookup_table)
+}
+
+pub fn day_05_part_2(data: &str) -> i64 {
+    let (_, data) = parse_input_data(data).expect("Failed to parse input data");
+
+    let faster_rules = build_faster_rules(&data.rules);
 
     data.updates
         .iter()
         .filter(|update| !is_valid_update(update, &faster_rules))
         .map(|update| {
+            let (_, sort_lookup_table) = kahn_algorithm(update, &data.rules);
             let sorted_update = sort_with_lookup_table(update, &sort_lookup_table);
             sorted_update[update.len() / 2] as i64
         })
