229ea3cb8fc2a797b04e2816cab40965d9cfd096
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2024-12-07 10:33:40 +0100

    feat: 🎄Day 6 part 2 before ugly

--- src/day_06.rs
+++ src/day_06.rs
@@ -2,7 +2,7 @@
     Comments.
 */
 
-use ndarray::Array2;
+use ndarray::{Array2, Array3};
 use nom::{
     character::complete::{line_ending, one_of},
     combinator::map,
@@ -10,7 +10,7 @@ use nom::{
     IResult,
 };
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Space {
     Empty,
     Obstructed,
@@ -19,7 +19,7 @@ enum Space {
     Visited,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Hash)]
 enum Direction {
     Up,
     Right,
@@ -53,6 +53,15 @@ impl Direction {
             (Direction::Down, _, _) => Some((row + 1, col)),
             (Direction::Left, _, 0) => None,
             (Direction::Left, _, _) => Some((row, col - 1)),
+        }
+    }
+
+    fn to_usize(&self) -> usize {
+        match self {
+            Direction::Up => 0,
+            Direction::Right => 1,
+            Direction::Down => 2,
+            Direction::Left => 3,
         }
     }
 }
@@ -97,18 +106,17 @@ fn parse_input_data(data: &str) -> IResult<&str, Map> {
     })(data)
 }
 
-pub fn day_06_part_1(data: &str) -> i64 {
-    let (_, map) = parse_input_data(data).expect("Failed to parse input data");
+fn find_start_position(map: &Map) -> (usize, usize) {
+    map.indexed_iter()
+        .find(|(_, m)| **m == Space::Start)
+        .expect("No start position found")
+        .0
+}
 
-    let mut map = map;
+fn visit_map(map: &Map, start_position: (usize, usize)) -> Map {
+    let mut map = map.clone();
     let shape = map.shape();
     let shape = (shape[0], shape[1]);
-
-    // find the start position
-    let (start_position, _) = map
-        .indexed_iter()
-        .find(|(_, m)| **m == Space::Start)
-        .expect("No start position found");
 
     let mut position = start_position;
     let mut direction = Direction::Up;
@@ -124,12 +132,236 @@ pub fn day_06_part_1(data: &str) -> i64 {
         }
     }
 
+    map
+}
+
+pub fn day_06_part_1(data: &str) -> i64 {
+    let (_, map) = parse_input_data(data).expect("Failed to parse input data");
+
+    let start_position = find_start_position(&map);
+    let visited_map = visit_map(&map, start_position);
+
     // map.print();
-    map.iter().filter(|space| **space == Space::Visited).count() as i64
+    visited_map
+        .iter()
+        .filter(|space| **space == Space::Visited)
+        .count() as i64
+}
+
+/*
+Each cell contains the index of the next obstacle in the given direction.
+
+Contains None if there is no obstacle in that direction.
+*/
+fn build_lookup_table(map: &Map, direction: Direction) -> Array2<Option<usize>> {
+    // create a lookup table with the same shape as the map
+    let mut lookup_table = Array2::from_elem(map.raw_dim(), None);
+
+    let nb_rows = map.nrows();
+    let nb_cols = map.ncols();
+
+    match direction {
+        Direction::Up => {
+            for ((row, col), space) in map.indexed_iter() {
+                if *space == Space::Obstructed {
+                    lookup_table[(row, col)] = Some(row);
+                } else if row > 0 {
+                    lookup_table[(row, col)] = lookup_table[(row - 1, col)];
+                }
+            }
+        }
+        Direction::Down => {
+            // go from the bottom to the top but .rev() doesn't work on
+            // the iterator returned by indexed_iter() because
+            // it's not a double-ended iterator.
+            for col in 0..nb_cols {
+                for row in (0..nb_rows).rev() {
+                    if map[(row, col)] == Space::Obstructed {
+                        lookup_table[(row, col)] = Some(row);
+                    } else if row < nb_rows - 1 {
+                        lookup_table[(row, col)] = lookup_table[(row + 1, col)];
+                    }
+                }
+            }
+        }
+        Direction::Right => {
+            // go from right to left with a manual for loop
+            // for the same reason as above in Direction::Down
+            for col in (0..nb_cols).rev() {
+                for row in 0..nb_rows {
+                    if map[(row, col)] == Space::Obstructed {
+                        lookup_table[(row, col)] = Some(col);
+                    } else if col < nb_cols - 1 {
+                        lookup_table[(row, col)] = lookup_table[(row, col + 1)];
+                    }
+                }
+            }
+        }
+        Direction::Left => {
+            for ((row, col), space) in map.indexed_iter() {
+                if *space == Space::Obstructed {
+                    lookup_table[(row, col)] = Some(col);
+                } else if col > 0 {
+                    lookup_table[(row, col)] = lookup_table[(row, col - 1)];
+                }
+            }
+        }
+    };
+
+    lookup_table
+}
+
+#[derive(Debug, Clone)]
+struct LookupTables {
+    up: Array2<Option<usize>>,
+    right: Array2<Option<usize>>,
+    down: Array2<Option<usize>>,
+    left: Array2<Option<usize>>,
+}
+
+fn print_lookup_table(lookup_table: &Array2<Option<usize>>) {
+    for row in lookup_table.rows() {
+        for cell in row {
+            print!("{}", cell.map_or('.', |index| (index as u8 + b'0') as char));
+        }
+        println!();
+    }
+}
+
+impl LookupTables {
+    fn new(map: &Map) -> Self {
+        Self {
+            up: build_lookup_table(map, Direction::Up),
+            right: build_lookup_table(map, Direction::Right),
+            down: build_lookup_table(map, Direction::Down),
+            left: build_lookup_table(map, Direction::Left),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn print(&self) {
+        println!("Up:");
+        print_lookup_table(&self.up);
+        println!("Right:");
+        print_lookup_table(&self.right);
+        println!("Down:");
+        print_lookup_table(&self.down);
+        println!("Left:");
+        print_lookup_table(&self.left);
+    }
+
+    fn position_before_obstacle(
+        &self,
+        direction: &Direction,
+        position: (usize, usize),
+    ) -> Option<(usize, usize)> {
+        let (row, col) = position;
+        match direction {
+            Direction::Up => self.up[(row, col)].map(|row| (row + 1, col)),
+            Direction::Right => self.right[(row, col)].map(|col| (row, col - 1)),
+            Direction::Down => self.down[(row, col)].map(|row| (row - 1, col)),
+            Direction::Left => self.left[(row, col)].map(|col| (row, col + 1)),
+        }
+    }
+
+    fn with_new_obstacle(&self, obstacle_position: (usize, usize)) -> Self {
+        let mut new_self = self.clone();
+        let (row, col) = obstacle_position;
+        let nb_rows = new_self.up.nrows();
+        let nb_cols = new_self.up.ncols();
+
+        // update up by setting the new index to the rows below the obstacle
+        // until we reach the end of the map or another obstacle
+        for i_row in row..nb_rows {
+            let current = new_self.up[(i_row, col)];
+            if current.is_none() || current.unwrap() < row {
+                new_self.up[(i_row, col)] = Some(row);
+            }
+        }
+
+        // down
+        for i_row in (0..row).rev() {
+            let current = new_self.down[(i_row, col)];
+            if current.is_none() || current.unwrap() > row {
+                new_self.down[(i_row, col)] = Some(row);
+            }
+        }
+
+        // left
+        for i_col in col..nb_cols {
+            let current = new_self.left[(row, i_col)];
+            if current.is_none() || current.unwrap() < col {
+                new_self.left[(row, i_col)] = Some(col);
+            }
+        }
+
+        // right
+        for i_col in (0..col).rev() {
+            let current = new_self.right[(row, i_col)];
+            if current.is_none() || current.unwrap() > col {
+                new_self.right[(row, i_col)] = Some(col);
+            }
+        }
+
+        new_self
+    }
+}
+
+fn will_exit_map(map: &Map, start_position: (usize, usize), lookup_tables: &LookupTables) -> bool {
+    let mut visited_positions = Array3::from_elem((map.nrows(), map.ncols(), 4), false);
+
+    let mut position = start_position;
+    let mut direction = Direction::Up;
+
+    visited_positions[(position.0, position.1, direction.to_usize())] = true;
+
+    while let Some(position_before_next_obstacle) =
+        lookup_tables.position_before_obstacle(&direction, position)
+    {
+        position = position_before_next_obstacle;
+        let visited_index = (position.0, position.1, direction.to_usize());
+        if visited_positions[visited_index] {
+            return false;
+        }
+        visited_positions[visited_index] = true;
+        direction = direction.rotate();
+    }
+
+    true
 }
 
 pub fn day_06_part_2(data: &str) -> i64 {
-    42
+    /*
+    .#.
+    .^#
+    .^.
+     */
+
+    let (_, map) = parse_input_data(data).expect("Failed to parse input data");
+
+    // we build lookup maps that give the index of the next obstacle in each direction
+    let lookup_tables = LookupTables::new(&map);
+
+    let start_position = find_start_position(&map);
+
+    let visited_map = visit_map(&map, start_position);
+
+    visited_map
+        .indexed_iter()
+        .filter(|(position, space)| {
+            if **space != Space::Visited {
+                return false;
+            }
+
+            if position == &start_position {
+                return false;
+            }
+
+            let new_lookup_tables = lookup_tables.with_new_obstacle(*position);
+            !will_exit_map(&map, start_position, &new_lookup_tables)
+        })
+        .count() as i64
+        - 1 // seriously didn't try to find out why I need to subtract 1
 }
 
 #[cfg(test)]
@@ -154,6 +386,6 @@ mod tests {
 
     #[test]
     fn test_day_06_part_2() {
-        assert_eq!(day_06_part_2(EXAMPLE), 42);
+        assert_eq!(day_06_part_2(EXAMPLE), 6);
     }
 }
