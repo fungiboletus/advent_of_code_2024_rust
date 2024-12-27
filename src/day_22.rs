3eda6fd9103f9ba0b991e2534e07231f476ee463
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2024-12-27 20:20:59 +0100

    feat: 🎄Day 22 part 2

--- src/day_22.rs
+++ src/day_22.rs
@@ -2,6 +2,11 @@
     Comments.
 */
 
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex, RwLock},
+};
+
 use nom::{character::complete::line_ending, multi::separated_list0, IResult};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
@@ -55,19 +60,158 @@ pub fn day_22_part_1(data: &str) -> i64 {
         .sum()
 }
 
+#[inline]
+fn sequence_to_number(a: i8, b: i8, c: i8, d: i8) -> u32 {
+    static MASK: u32 = 0b11111;
+    ((((a + 9) as u32) & MASK) << 15)
+        | ((((b + 9) as u32) & MASK) << 10)
+        | ((((c + 9) as u32) & MASK) << 5)
+        | (((d + 9) as u32) & MASK)
+}
+
+#[inline]
+fn last_digit_base_10(number: u32) -> i8 {
+    (number % 10) as i8
+}
+
 pub fn day_22_part_2(data: &str) -> i64 {
-    42
+    let (_, data) = parse_input_data(data).expect("Failed to parse input data");
+
+    //let mut sums: HashMap<u32, i64> = HashMap::new();
+    //let mut highest_sum: i64 = 0;
+
+    // make it thread safe
+    //let sums: Arc<RwLock<HashMap<u32, i64>>> = Arc::new(RwLock::new(HashMap::new()));
+    let sums: Arc<RwLock<Vec<i64>>> = Arc::new(RwLock::new(vec![0; 1 << 20]));
+    let highest_sum: Arc<RwLock<i64>> = Arc::new(RwLock::new(0));
+
+    data.par_iter().for_each(|seed| {
+        let sums = Arc::clone(&sums);
+        let highest_sum = Arc::clone(&highest_sum);
+        let secret_g3 = *seed;
+        let secret_g2 = compute_next_secret(secret_g3);
+        let secret_g1 = compute_next_secret(secret_g2);
+        let mut secret_g0 = compute_next_secret(secret_g1);
+
+        let last_digit_g3 = last_digit_base_10(secret_g3);
+        let last_digit_g2 = last_digit_base_10(secret_g2);
+        let last_digit_g1 = last_digit_base_10(secret_g1);
+        let mut last_digit_g0 = last_digit_base_10(secret_g0);
+
+        let mut diff_3_to_2 = last_digit_g2 - last_digit_g3;
+        let mut diff_2_to_1 = last_digit_g1 - last_digit_g2;
+        let mut diff_1_to_0 = last_digit_g0 - last_digit_g1;
+
+        let max_iter = 2000 - 3;
+
+        let mut map: HashMap<u32, i8> = HashMap::new();
+        //let mut sums: Vec<i8> = vec![0; 1 << 20];
+        //let mut map: Vec<Option<i8>> = vec![None; 1 << 20];
+
+        for i in 0..max_iter {
+            let next = compute_next_secret(secret_g0);
+            let last_digit_next = last_digit_base_10(next);
+            let diff_0_to_next = last_digit_next - last_digit_g0;
+
+            // let diff_2_to_1 = last_digit_base_10(secret_g1) - last_digit_base_10(secret_g2);
+            // let diff_1_to_0 = last_digit_base_10(secret_g0) - last_digit_base_10(secret_g1);
+            // let diff_0_to_next = last_digit_base_10(next) - last_digit_base_10(secret_g0);
+
+            //println!(
+            //    "{}: {} {} {} {} {}",
+            //    i, next, secret_g0, secret_g1, secret_g2, secret_g3
+            //);
+            //println!(
+            //    "{}: {} {} {} {}",
+            //    i, diff_3_to_2, diff_2_to_1, diff_1_to_0, diff_0_to_next
+            //);
+            let number = sequence_to_number(diff_3_to_2, diff_2_to_1, diff_1_to_0, diff_0_to_next);
+
+            // unsert if not exist in one line
+            //map.entry(number).or_insert(last_digit_next);
+            if let std::collections::hash_map::Entry::Vacant(e) = map.entry(number) {
+                e.insert(last_digit_next);
+                let new_sum_b: i64;
+                {
+                    let mut sums = sums.write().unwrap();
+                    let new_sum = sums[number as usize] + last_digit_next as i64;
+                    sums[number as usize] = new_sum;
+                    new_sum_b = new_sum;
+                }
+                let mut should_update = false;
+                {
+                    let highest_sum_read = highest_sum.read().unwrap();
+                    if new_sum_b > *highest_sum_read {
+                        should_update = true;
+                    }
+                }
+                if should_update {
+                    let mut highest_sum = highest_sum.write().unwrap();
+                    *highest_sum = new_sum_b;
+                }
+
+                /*sums.entry(number)
+                .and_modify(|sum| {
+                    let new_sum = *sum + last_digit_next as i64;
+                    let mut should_update = false;
+                    {
+                        let highest_sum_read = highest_sum.read().unwrap();
+                        if new_sum > *highest_sum_read {
+                            should_update = true;
+                        }
+                    }
+                    if should_update {
+                        let mut highest_sum = highest_sum.write().unwrap();
+                        *highest_sum = new_sum;
+                    }
+                    *sum = new_sum;
+                })
+                .or_insert(last_digit_next as i64);*/
+            }
+            /*if map[number as usize].is_none() {
+                map[number as usize] = Some(last_digit_next);
+            }*/
+
+            //secret_g3 = secret_g2;
+            //secret_g2 = secret_g1;
+            //secret_g1 = secret_g0;
+            secret_g0 = next;
+
+            //last_digit_g3 = last_digit_g2;
+            //last_digit_g2 = last_digit_g1;
+            //last_digit_g1 = last_digit_g0;
+            last_digit_g0 = last_digit_next;
+
+            diff_3_to_2 = diff_2_to_1;
+            diff_2_to_1 = diff_1_to_0;
+            diff_1_to_0 = diff_0_to_next;
+
+            //println!("{}: {}", i, number);
+        }
+        //println!("debug: {}", sequence_to_number(9, 9, 9, 9));
+        //println!("map: {:?}", map);
+    });
+
+    //println!("sums: {:?}", sums);
+    //println!("highest_sum: {}", highest_sum);
+    //highest_sum.lock().unwrap().clone()
+    let highest_sum = highest_sum.read().unwrap();
+    *highest_sum
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "1
+    const EXAMPLE_PART_1: &str = "1
 10
 100
-2024
-";
+2024";
+
+    const EXAMPLE_PART_2: &str = "1
+2
+3
+2024";
 
     #[test]
     fn test_day_22_mix() {
@@ -95,11 +239,11 @@ mod tests {
 
     #[test]
     fn test_day_22_part_1() {
-        assert_eq!(day_22_part_1(EXAMPLE), 37327623);
+        assert_eq!(day_22_part_1(EXAMPLE_PART_1), 37327623);
     }
 
     #[test]
     fn test_day_22_part_2() {
-        assert_eq!(day_22_part_2(EXAMPLE), 42);
+        assert_eq!(day_22_part_2(EXAMPLE_PART_2), 23);
     }
 }
